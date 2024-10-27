43d0ab8517ac603168a03f6b6fc1d0c01e2d7d3e
strlla4536 <strlla4536@gmail.com>
2024-10-27 20:50:34 +0900

    [feat] 폰트 추가

--- MalatangChallenge/ContentView.swift
+++ MalatangChallenge/ContentView.swift
@@ -8,17 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    ZStack{
+      Image("mainImg")
+        .resizable()
+        .scaledToFill()
+        .frame(width: 393, height: 900)
+      
+      Spacer()
+      
+      ZStack(alignment: .center){
+        Rectangle()
+          .foregroundColor(Color(red: 0.98, green: 0.78, blue: 0.78))
+          .frame(width: 240, height: 83.91608)
+          .cornerRadius(167.83217)
+        
+        Text("START")
+          .font(.custom("DNFBitBitv2", size: 30))
+      }
+      .padding(.bottom, 307)
+      .border(.blue)
+      
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
