bbf99f3f3630b28563eac230a414468905b66ec4
chosun-sangnom <kj82076@gmail.com>
2024-10-30 03:48:54 +0900

    ContentView에서 NavigationStack을 appState.navigationPath로 설정하여 최상위에서 흐름제어 ResultView에서 EndingView로 appState의 navigateToEndingView(score:)를 호출해 경로를 설정하고, fullScreenCover->NavigationStack을 통해 이동 AppState 클래스 추가(NavigationPath) 결과값에 따른 ResultView 뷰 구현 EndingView의 처음으로 버튼 구현 에셋추가

--- MalatangChallenge/ContentView.swift
+++ MalatangChallenge/ContentView.swift
@@ -8,35 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-  var body: some View {
-    NavigationStack {
-      ZStack{
-        Image("HomeView")
-          .resizable()
-          .scaledToFill()
-          .ignoresSafeArea(.all)
-        
-        NavigationLink(destination: LoadingView()) {
-          VStack {
-            Spacer()
-            ZStack(alignment: .center){
-              Rectangle()
-                .foregroundColor(Color(red: 0.98, green: 0.78, blue: 0.78))
-                .frame(width: 240, height: 83.91608)
-                .cornerRadius(167.83217)
-              
-              Text("START")
-                .font(.custom("DNFBitBitv2", size: 30))
-                .foregroundStyle(Color.black)
+    @EnvironmentObject var appState: AppState
+    
+    var body: some View {
+        NavigationStack (path: $appState.navigationPath) {
+            ZStack{
+                Image("HomeView")
+                    .resizable()
+                    .scaledToFill()
+                    .ignoresSafeArea(.all)
+                
+                NavigationLink(destination: LoadingView()) {
+                    VStack {
+                        Spacer()
+                        ZStack(alignment: .center){
+                            Rectangle()
+                                .foregroundColor(Color(red: 0.98, green: 0.78, blue: 0.78))
+                                .frame(width: 240, height: 83.91608)
+                                .cornerRadius(167.83217)
+                            
+                            Text("START")
+                                .font(.custom("DNFBitBitv2", size: 30))
+                                .foregroundStyle(Color.black)
+                        }
+                        .padding(.bottom, 307)
+                    }
+                }
             }
-            .padding(.bottom, 307)
-          }
+            .navigationDestination(for: Int.self) { score in
+                EndingView(score: score)
+            }
         }
-      }
     }
-  }
 }
-
 #Preview {
   ContentView()
 }
--- MalatangChallenge/MalatangChallengeApp.swift
+++ MalatangChallenge/MalatangChallengeApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct MalatangChallengeApp: App {
+    @StateObject private var appState = AppState()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(appState)
         }
     }
 }
