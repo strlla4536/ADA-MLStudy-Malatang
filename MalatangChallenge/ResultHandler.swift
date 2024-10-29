01f79c9a19a520c10d6d47a3c8b5d727732af09b
chosun-sangnom <kj82076@gmail.com>
2024-10-29 12:00:05 +0900

    [feat] 기능 추가 - 안 올라간 것들 추가

--- MalatangChallenge/ResultHandler.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ResultHandler.swift
-//  MalatangChallenge
-//
-//  Created by 김준수(엘빈) on 10/28/24.
-//
-
-import Foundation
--- MalatangChallenge/ResultView.swift
+++ MalatangChallenge/ResultView.swift
@@ -5,4 +5,26 @@
 //  Created by 김준수(엘빈) on 10/28/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ResultView: View {
+    let score: Int
+    @State private var navigateToEndingView = false
+
+    var body: some View {
+        VStack {
+            Text("Your Score: \(score)")
+                .font(.largeTitle)
+                .padding()
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                navigateToEndingView = true
+            }
+        }
+        .fullScreenCover(isPresented: $navigateToEndingView) {
+            EndingView(score: score)
+        }
+    }
+}
+
